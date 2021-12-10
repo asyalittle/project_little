fbf2af18668509385ea11fdb59960430d9b0c975
asyalittle <littam18@wfu.edu>
2021-12-10 01:56:32 +0000

    Updated Article, added References

--- _pipeline.R
+++ _pipeline.R
@@ -21,28 +21,28 @@ pacman::p_load(citr, tidyverse, rmarkdown, knitr, sessioninfo)
 
 # 1. Acquire data
 
-render_site(input = "analysis/1_acquire_data.Rmd")
+render_site(input = "../analysis/1_acquire_data.Rmd")
 
 # 2. Curate dataset
 
-render_site(input = "analysis/2_curate_dataset.Rmd")
+render_site(input = "../analysis/2_curate_dataset.Rmd")
 
 # 3. Transform dataset
 
-render_site(input = "analysis/3_transform_dataset.Rmd")
+render_site(input = "../analysis/3_transform_dataset.Rmd")
 
 # 4. Analyze dataset
 
-render_site(input = "analysis/4_analyze_dataset.Rmd")
+render_site(input = "../analysis/4_analyze_dataset.Rmd")
 
 # 5. Generate reports
 
-render_site(input = "analysis/5_generate_article.Rmd")
-render(input = "analysis/5_generate_article.Rmd", output_format = "bookdown::pdf_document2", output_file = "article.pdf")
+render_site(input = "../analysis/5_generate_article.Rmd")
+render(input = "../analysis/5_generate_article.Rmd", output_format = "bookdown::pdf_document2", output_file = "article.pdf")
 
 # Create index: main website splash page
 
-render_site(input = "analysis/index.Rmd")
+render_site(input = "../analysis/index.Rmd")
 
 # CLEAN UP ----------------------------------------------------------------
 
